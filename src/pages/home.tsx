8593bd51a1cc8b182b226b841cca693f23ce719d
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-04-22 17:03:28 +0530

    made the hero section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,34 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Eye,EyeClosed } from "lucide-react";
+import { Eye, EyeClosed } from "lucide-react";
 import { useState } from "react";
-function focusInp(){
-const cont = document.getElementById("cont");
-cont.style.backgroundColor = "rgb(8, 8, 8)"
+function focusInp() {
+  const cont = document.getElementById("cont");
+  cont.style.backgroundColor = "rgb(8, 8, 8)";
 }
-function notFocusInp(){
-const cont = document.getElementById("cont");
-cont.style.backgroundColor = "rgb(15, 17, 18)"
 
+function notFocusInp() {
+  const cont = document.getElementById("cont");
+  cont.style.backgroundColor = "rgb(15, 17, 18)";
 }
 
 const Home = () => {
+  return (
+    <div className="w-full min-h-screen bg-[url('https://cloud-4ze8a6fmk-hack-club-bot.vercel.app/0bg.jpeg')] bg-cover bg-center bg-no-repeat bg-fixed">
+      {/* Hero Section */}
+      <div className="w-full h-screen flex items-center justify-center ">
+        <div className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+          <div className="custom-outline">Hacklumina'25</div>
+        </div>
+      </div>
 
-    return(
-<div className="w-full min-h-screen flex items-center justify-center bg-[url('https://cloud-4ze8a6fmk-hack-club-bot.vercel.app/0bg.jpeg')] bg-cover bg-center bg-no-repeat">
-  <div className="text-white text-6xl"><h1 className="custom-outline">Hacklumina'25</h1></div>
-</div>
+      {/* Next Section */}
+      <div className="h-screen flex items-center justify-center">
+        <p className="text-4xl text-white">Hello</p>
+      </div>
+    </div>
+  );
+};
 
-
-
-    );
-}
 export default Home;
