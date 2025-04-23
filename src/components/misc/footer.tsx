827da3c7518997b6af51f4508fbf9fb3714d8d19
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-04-23 22:01:10 +0530

    made a minimal footer

--- src/components/misc/footer.tsx
+++ src/components/misc/footer.tsx
@@ -1,6 +1,13 @@
+import Link from "./link";
+import Github from "./github";
+import Img from "./img";
 const Footer = () =>{
-    return(<div className="h-16 bg-gray-900">
-
+    return(<div className="h-36 w-full flex flex-col shadow-md bg-white bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-40 backdrop-saturate-100 backdrop-contrast-100 custom-section-border">
+        <div className="flex flex-row gap-6 justify-center">
+        <p>HackLumina</p>
+        <p></p>
+        </div>
+        <p className="pr-4 pl-4 text-[0.7rem] break-normal absolute">This site is open-sourced here! <br></br> Shoot an email to Manan if you have any issues.</p>
     </div>);
 }
 
